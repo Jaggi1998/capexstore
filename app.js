44a7c4dec9419fa5faec49ade8a12bb43435b889
jaggi1998 <jagjots28@gmail.com>
2022-04-17 22:33:08 +0530

    newCode

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const path = require ("path");
 const app = express()
 
-const port = 4000; 
+const port = 5000; 
 
 const staticPath = path.join(__dirname, "./public")
 app.use(express.static(staticPath))
