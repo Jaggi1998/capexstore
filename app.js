5a8afe44012286cce20edc4cf48374706e82698d
jaggi1998 <jagjots28@gmail.com>
2022-04-22 15:12:06 +0530

    newCode

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = express()
 
 const port = 3000; 
 
-const staticPath = path.join(__dirname, "./public")
-app.use(express.static(staticPath))
+// const staticPath = path.join(__dirname, "./public")
+// app.use(express.static(staticPath))
 
-app.get("/", (req, res) =>{
-    res.send("hello express server")
-});
+// Access public folder from root
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/images',express.static(path.join(__dirname, 'public/assets/images')));
+app.use('/css', express.static(path.join(__dirname, 'public/assets/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/assets/js')));
+
+app.set("view engine", "ejs")
+
+app.get('/', (req,res)=>{res.render('index')})
+
+app.get('/blog', (req,res)=>{res.render('blog')})
+app.get('*', (req,res)=>{res.render('404')})
+
+
+
 
 
 app.listen (port, ()=>{
